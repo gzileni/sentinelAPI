9c8731beb2ed3d779041f645ebbe3738c23ec3e3
gzileni <giuseppe.zileni@gmail.com>
2021-02-07 22:36:41 +0100

    update views

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+var createError = require('http-errors');
 
-const indexRouter = require('./routes/index');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/api.json');
+
 const sentinelRouter_v1 = require('./routes/sentinel');
 const processRouter_v1 = require('./routes/process');
+const indexRouter = require('./routes/index');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -22,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'evalscripts')));
 
 app.use('/', indexRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/v1/sentinel', sentinelRouter_v1);
 app.use('/api/v1/process', processRouter_v1);
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
 var dotenv = require('dotenv');
 dotenv.config();
 
+var log = require('logbootstrap');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: process.env.TITLE 
-  });
+router.get('/', (req, res, next) => {
+    res.render('index', { 
+        title: process.env.TITLE || 'sentinel5P API'
+    })
 });
 
 module.exports = router;
